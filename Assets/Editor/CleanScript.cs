57ec210c5d83dfe71a051d948d52e186169eb13a
Sticklime <91189001+Sticklime@users.noreply.github.com>
2024-04-07 00:37:24 +0300

    CleanMissingScript

---
+++ Assets/Editor/CleanScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class CleanMissingScript : EditorWindow
+    {
+        [MenuItem("Auto/CleanMissingScript")]
+        private static void FindAndRemoveMissingInSelected()
+        {
+            var deepSelection = EditorUtility.CollectDeepHierarchy(Selection.gameObjects);
+            var compCount = 0;
+            var goCount = 0;
+            
+            foreach (var script in deepSelection)
+            {
+                if (script is not GameObject currentGameObject) 
+                    continue;
+                
+                var count = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(currentGameObject);
+
+                if (count <= 0)
+                    continue;
+                
+                Undo.RegisterCompleteObjectUndo(currentGameObject, "Remove missing scripts");
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(currentGameObject);
+                compCount += count;
+                goCount++;
+            }
+
+            Debug.Log($"Found and removed {compCount} missing scripts from {goCount} GameObjects");
+        }
+    }
+}
